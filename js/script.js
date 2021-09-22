b692249120a899b60b3434bfd113c0d668565f22
ilya <ilia.podrezov2001@gmail.com>
2021-09-22 16:15:57 +0300

    second task completed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 console.log(numberOfFilms);
 console.log(typeof(numberOfFilms));
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log(personalMovieDB);

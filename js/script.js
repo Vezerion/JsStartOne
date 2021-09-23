d5b0b860133e464024461636030df60a429aef75
ilya <ilia.podrezov2001@gmail.com>
2021-09-23 17:22:47 +0300

    part 2, second task completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 console.log(numberOfFilms);
 
 const personalMovieDB = {
@@ -20,10 +20,26 @@ console.log(personalMovieDB);
 // personalMovieDB.movies[a] = b;
 // personalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++){
-    let a = prompt("Один из послдених просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", "");
-    personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++)
+{
+    const a = prompt("Один из послдених просмотренных фильмов?", ''),
+          b = prompt("На сколько оцените его?", '');
+
+    // if(a != null && b != null && a != '' && b != '' && a.length < 50){
+    //     personalMovieDB.movies[a] = b;
+    //     console.log('done');
+    // }
+    // else {
+    //     console.log('error');
+    //     i--;
+    // }
+    if (a == null || b == null || a.length > 50 || a == '' || b == ''){
+        i--;
+    }
+    else {
+        personalMovieDB.movies[a] = b;
+    }
 }
+
 
 console.log(personalMovieDB.movies);
